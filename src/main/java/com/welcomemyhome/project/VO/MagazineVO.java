71c43fdafacfc292bbfb652a479b050740e59aaa
nocucumberno <nocucumberno@users.noreply.github.com>
2018-12-04 03:33:59 +0900

    jun

--- src/main/java/com/welcomemyhome/project/VO/MagazineVO.java
+++ src/main/java/com/welcomemyhome/project/VO/MagazineVO.java
@@ -1,5 +1,63 @@
 package com.welcomemyhome.project.VO;
 
 public class MagazineVO {
+	private int magazine_idx;
+	private String magazine_title;
+	private String magazine_post_date;
+	private String magazine_picture_path;
+	private String magazine_thumbnail_path;
+	private int magazine_like_count;
+	private int magazine_comment_count;
+	private int magazine_hit_count;
+	
+	public int getMagazine_idx() {
+		return magazine_idx;
+	}
+	public void setMagazine_idx(int magazine_idx) {
+		this.magazine_idx = magazine_idx;
+	}
+	public String getMagazine_title() {
+		return magazine_title;
+	}
+	public void setMagazine_title(String magazine_title) {
+		this.magazine_title = magazine_title;
+	}
+	public String getMagazine_post_date() {
+		return magazine_post_date;
+	}
+	public void setMagazine_post_date(String magazine_post_date) {
+		this.magazine_post_date = magazine_post_date;
+	}
+	public String getMagazine_picture_path() {
+		return magazine_picture_path;
+	}
+	public void setMagazine_picture_path(String magazine_picture_path) {
+		this.magazine_picture_path = magazine_picture_path;
+	}
+	public String getMagazine_thumbnail_path() {
+		return magazine_thumbnail_path;
+	}
+	public void setMagazine_thumbnail_path(String magazine_thumbnail_path) {
+		this.magazine_thumbnail_path = magazine_thumbnail_path;
+	}
+	public int getMagazine_like_count() {
+		return magazine_like_count;
+	}
+	public void setMagazine_like_count(int magazine_like_count) {
+		this.magazine_like_count = magazine_like_count;
+	}
+	public int getMagazine_comment_count() {
+		return magazine_comment_count;
+	}
+	public void setMagazine_comment_count(int magazine_comment_count) {
+		this.magazine_comment_count = magazine_comment_count;
+	}
+	public int getMagazine_hit_count() {
+		return magazine_hit_count;
+	}
+	public void setMagazine_hit_count(int magazine_hit_count) {
+		this.magazine_hit_count = magazine_hit_count;
+	}
+
 
 }
