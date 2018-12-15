3e24219476c75a208f9a348ced387c2bd82fa0da
guswkpro <guswk_pro@naver.com>
2018-12-15 14:36:42 +0900

    adwsf

--- src/main/java/com/welcomemyhome/project/Controller/MagazineController.java
+++ src/main/java/com/welcomemyhome/project/Controller/MagazineController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Locale;
 
 import javax.inject.Inject;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
@@ -13,28 +14,37 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.ServletContextAware;
 
 import com.welcomemyhome.project.Service.MagazineService;
 import com.welcomemyhome.project.VO.MagazineVO;
 
 @Controller
-public class MagazineController {
+public class MagazineController implements ServletContextAware{
 	private static final Logger logger = LoggerFactory.getLogger(MagazineController.class);
 
 	@Inject
 	private MagazineService service;
+
+	private ServletContext servletContext;
 	
 	@RequestMapping(value = "/magazine", method = RequestMethod.GET)
 	public String Magazine(Locale locale, Model model, @RequestParam int offset, HttpSession session) throws Exception {
 
 		logger.info("Magazine");
 		
-		System.out.println(session.getAttribute("token")+"ASDFASDFASDF");
+		System.out.println(servletContext.getRealPath("WEB-INF/views/public/") + "ASDFASDFASDFASDFASDF");
 
 		List<MagazineVO> MagazineList = service.selectMagazine(offset);
 
 		model.addAttribute("MagazineList", MagazineList);
 
 		return "magazine";
+	}
+
+	@Override
+	public void setServletContext(ServletContext servletContext) {
+		// TODO Auto-generated method stub
+		this.servletContext = servletContext;		
 	}
 }
--- src/main/java/com/welcomemyhome/project/Service/MagazineServiceImpl.java
+++ src/main/java/com/welcomemyhome/project/Service/MagazineServiceImpl.java
@@ -1,23 +1,40 @@
 package com.welcomemyhome.project.Service;
 
+import java.io.File;
+
 import java.util.List;
 
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
+import org.apache.commons.codec.binary.Base64;
 
 import com.welcomemyhome.project.DAO.MagazineDAO;
 import com.welcomemyhome.project.VO.MagazineVO;
 
 @Service
 public class MagazineServiceImpl implements MagazineService {
- 
-    @Inject
-    private MagazineDAO dao;
-    
-    @Override
-    public List<MagazineVO> selectMagazine(int offset) throws Exception {
- 
-        return dao.selectMagazine(offset);
-    }
+
+	@Inject
+	private MagazineDAO dao;
+
+	@Override
+	public List<MagazineVO> selectMagazine(int offset) throws Exception {
+		List<MagazineVO> magazineList = dao.selectMagazine(offset);
+
+		System.out.println(magazineList.get(0).getMagazine_thumbnail_path());
+		File f = new File(magazineList.get(0).getMagazine_thumbnail_path());
+		byte[] content = new byte[(int) f.length()];
+		System.out.println("인코딩 text : " + new String(content) + f.length());
+
+		/* base64 encoding */
+		byte[] encodedBytes = Base64.encodeBase64(content);
+		/* base64 decoding */
+		byte[] decodedBytes = Base64.decodeBase64(encodedBytes);
+
+		System.out.println("인코딩 text : " + new String(encodedBytes));
+		System.out.println("디코딩 text : " + new String(decodedBytes));
+
+		return dao.selectMagazine(offset);
+	}
 }
